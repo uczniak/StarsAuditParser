5c2faa6b3c1b8adab82634dabd6d095d3c26a6a4
uczniak <superkobra84@gmail.com>
2016-10-31 20:51:24 +0000

    Tournament count by stakes added

--- StarsAuditParser.py
+++ StarsAuditParser.py
@@ -1,5 +1,11 @@
 from lxml.html import parse
 
+def print_count_by_stakes(cnt):
+    res = ""
+    for k, v in sorted(cnt.items()):
+        res += ' '*(max([len(str(k)) for k in cnt]) - len(str(k))) + str(k) + "$: " + str(v) + "\n"
+    return res
+
 while True:
     try:
         page = parse(input("Please enter audit file name (*.html): "))
@@ -14,6 +20,7 @@ for row in rows:
     data.append([c.text_content() for c in row.getchildren()])
 
 count = 0
+count_by_stakes = {}
 result = 0
 
 start_rb = float(data[2][13]
@@ -39,6 +46,8 @@ for row in data[2:]:
                       .replace('.','')
                       .replace(' ',''))/100
         result += delta
+        if delta < 0:
+            count_by_stakes[int(-delta)] = count_by_stakes.get(int(-delta), 0) + 1
     elif '(' in row[7]:
         rakeback += float(row[7]
                       .replace('(','')
@@ -49,6 +58,7 @@ for row in data[2:]:
 
 
 print("\nTotal number of Spin&Gos played: {}".format(count))
+print("Count by stakes:\n{}".format(print_count_by_stakes(count_by_stakes)))
 print("Total profit from Spin&Gos: ${:.2f}".format(result))
 print("Total rakeback from StarsCoins: ${:.2f}".format(rakeback))
 
